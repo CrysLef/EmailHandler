31cc39793c9146d0c5d55c8490ef44387d3eb621
Crystian Lefundes <crystian.lf@gmail.com>
2024-03-18 20:44:49 -0300

    feat: Add constructor with EmailDTO and functions to change email status

--- src/main/java/br/com/cryslefundes/emailHandler/core/entity/Email.java
+++ src/main/java/br/com/cryslefundes/emailHandler/core/entity/Email.java
@@ -1,16 +1,21 @@
 package br.com.cryslefundes.emailHandler.core.entity;
 
+import br.com.cryslefundes.emailHandler.core.dto.EmailDTO;
 import br.com.cryslefundes.emailHandler.core.enums.EmailStatus;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 
 @Entity
-@Table(name = "TB_EMAIL")
-@Data
+@Table(name = "emails")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Email {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -19,9 +24,25 @@ public class Email {
     private String emailFrom;
     private String emailTo;
     private String subject;
-    @Column(columnDefinition = "TEXT")
     private String body;
     @Enumerated(EnumType.STRING)
-    private EmailStatus emailStatus;
+    private EmailStatus status;
     private LocalDateTime sendDate;
+
+    public Email(EmailDTO dto) {
+        this.ownerRef = dto.ownerRef();
+        this.emailFrom = System.getenv("GMAIL_SENDER_USER");
+        this.emailTo = dto.emailTo();
+        this.subject = dto.subject();
+        this.body = dto.body();
+        this.sendDate = LocalDateTime.now();
+    }
+
+    public void markEmailStatusAsSent() {
+        this.status = EmailStatus.SENT;
+    }
+
+    public void markEmailStatusAsError() {
+        this.status = EmailStatus.ERROR;
+    }
 }
